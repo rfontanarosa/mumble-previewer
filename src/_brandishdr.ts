79429fd8a097d375ecbaf117bc091d815bfce6c0
Roberto Fontanarosa <robertofontanarosa@gmail.com>
2026-05-20 15:36:33 +0200

    brandishdr - refinements

--- src/_brandishdr.ts
+++ src/_brandishdr.ts
@@ -1,4 +1,5 @@
 import { Config, makeConfig } from "./types";
+import { replaceAll } from "./utils";
 
 const BRANDISHDR_CHAR_PAIRS: [string, number][] = [
   // [" ", 1],
@@ -13,12 +14,25 @@ const BRANDISHDR_CHAR_PAIRS: [string, number][] = [
   ["W", 10 + 2],
 ];
 
+const BRANDISHDR_REGEXES: [string | RegExp, string][] = [
+  [/\[WAIT\]/g, ""],
+  [/\[COLOR:.\]/g, ""],
+  [/\[SFX:.\]/g, ""],
+];
+
+const BRANDISHDR_TEXT_REPLACER = (text: string): string => {
+  text = text.replaceAll(/\[PAGE\]\n/g, "\r");
+  text = replaceAll(text, BRANDISHDR_REGEXES);
+  return text;
+};
+
 export const brandishdrConfig: Config = makeConfig({
   charLimit: 320,
   lineLimit: 3,
   boxClasses: ["psp-480x272", "brandishdr-box"],
   fontClass: "brandishdr-main-font",
   charWidthPairs: BRANDISHDR_CHAR_PAIRS,
+  replacer: BRANDISHDR_TEXT_REPLACER,
   languages: {
     it: { charWidthPairs: [["àèéòù", 6 + 2], ["ì", 3 + 2], ["È", 7 + 2]] }
   }
