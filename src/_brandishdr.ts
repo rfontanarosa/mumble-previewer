c25eacedf6c6412ac1ec2abbcb6efde4fa3496a2
Roberto Fontanarosa <robertofontanarosa@gmail.com>
2026-05-20 12:30:24 +0200

    brandishdr - complete previewer

--- src/_brandishdr.ts
+++ src/_brandishdr.ts
@@ -1,13 +1,25 @@
 import { Config, makeConfig } from "./types";
 
 const BRANDISHDR_CHAR_PAIRS: [string, number][] = [
-  ["ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz", 16],
+  // [" ", 1],
+  ["!',.:;Iil", 2 + 2],
+  ["\"()]`j", 3 + 2],
+  ["*-1[ ", 4 + 2],
+  ["^_frt", 5 + 2],
+  ["+<>ahknsuvxyz", 6 + 2],
+  ["#$=EFJKLZ¥bcdegopq", 7 + 2],
+  ["&/023456789?ABCDGHMNOPQRSTUVXY", 8 + 2],
+  ["%@Mmw", 9 + 2],
+  ["W", 10 + 2],
 ];
 
 export const brandishdrConfig: Config = makeConfig({
-  charLimit: 19 * 16,
+  charLimit: 320,
   lineLimit: 3,
   boxClasses: ["psp-480x272", "brandishdr-box"],
   fontClass: "brandishdr-main-font",
   charWidthPairs: BRANDISHDR_CHAR_PAIRS,
+  languages: {
+    it: { charWidthPairs: [["àèéòù", 6 + 2], ["ì", 3 + 2], ["È", 7 + 2]] }
+  }
 });
